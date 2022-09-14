ea3f77882ab48134ebb9e83a2d09b8eec436176d
Krisjanis Straupe <krisjanis.straupe@dataflow.lv>
2022-09-14 16:47:25 +0300

    "Initial commit"

---
+++ xamarin_forms_window/xamarin_forms_window/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_forms_window
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            send_button.Clicked += Send_button_Clicked;
+        }
+
+        private void Send_button_Clicked(object sender, EventArgs e)
+        {
+            var message = ievade_teksts.Text;
+            izmaina.Text = message;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
